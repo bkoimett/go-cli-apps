5af771c8b9bca8f0427e8a346cc6e419616ec776
bkoimett <benkoimet@gmail.com>
2025-06-03 10:54:32 +0300

    Initial Commit

---
+++ cli-guessing-game/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main(){
+	guess := 0
+	fmt.Println("Guess a number between 1-100")
+	fmt.Scan(&guess)
+
+	randomNum := rand.Intn(100) 
+	
+	fmt.Println("The random number is :")
+	fmt.Println(randomNum)
+	fmt.Println()
+
+	if guess == randomNum {
+		fmt.Println("Your guess was correct")
+	} else {
+		fmt.Println("Your guess was incorrect")
+	}
+}
